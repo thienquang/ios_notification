69e2dca1a6a2bb858a179124505989772f62fb26
Thien Le <thien.lequang@asnet.com.vn>
2018-03-21 20:37:19 +0700

    first setup schedule and make pizza local notifications.

--- HuliPizzaNotification/ViewController.swift
+++ HuliPizzaNotification/ViewController.swift
@@ -7,13 +7,51 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
   
+  var isGrantedNotificationAccess = false
+  
+  func makePizzaContent() -> UNMutableNotificationContent {
+    let content = UNMutableNotificationContent()
+    content.title = "A Timed Pizza Step"
+    content.body = "Making Pizza"
+    
+    content.userInfo = ["step": 0]
+    return content
+  }
+  
+  func addNotification(trigger: UNNotificationTrigger?, content: UNMutableNotificationContent, identifier: String) {
+    let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+    UNUserNotificationCenter.current().add(request) { (error) in
+      if error != nil {
+        print("error adding notification: \(error?.localizedDescription)")
+      }
+    }
+  }
+  
   @IBAction func schedulePizza(_ sender: UIButton) {
+    if isGrantedNotificationAccess {
+      let content = UNMutableNotificationContent()
+      content.title = "A Schedule Pzza."
+      content.body = "Time to make a Pizza!!!"
+      let unitFlags: Set<Calendar.Component> = [.minute, .hour, .second]
+      var date = Calendar.current.dateComponents(unitFlags, from: Date())
+      date.second = date.second ?? 0 + 15
+      
+      let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+      addNotification(trigger: trigger, content: content, identifier: "message.schedule")
+    }
   }
   
   @IBAction func makeAPizza(_ sender: UIButton) {
+    if isGrantedNotificationAccess {
+      let content = makePizzaContent()
+      
+      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+      addNotification(trigger: trigger, content: content, identifier: "message.pizza")
+    }
   }
   
   @IBAction func nextPizzaStep(_ sender: UIButton) {
@@ -33,7 +71,15 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+      self.isGrantedNotificationAccess = granted
+      
+      if !granted {
+        // add alert to complain to user
+      }
+    }
   }
   
   override func didReceiveMemoryWarning() {
