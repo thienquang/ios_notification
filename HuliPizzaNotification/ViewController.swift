468a2622a1da3e632c1cef8b6f0c8e28a4ca6e1d
Thien Le <thien.lequang@asnet.com.vn>
2018-03-21 19:55:48 +0700

    init app

--- HuliPizzaNotification/ViewController.swift
+++ HuliPizzaNotification/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBAction func schedulePizza(_ sender: UIButton) {
+  }
+  
+  @IBAction func makeAPizza(_ sender: UIButton) {
+  }
+  
+  @IBAction func nextPizzaStep(_ sender: UIButton) {
+  }
+  
+  
+  @IBAction func viewPendingNotifications(_ sender: UIButton) {
+  }
+  
+  @IBAction func viewDeliveredNotifications(_ sender: UIButton) {
+  }
+  
+  @IBAction func removeNotification(_ sender: UIButton) {
+  }
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
 
